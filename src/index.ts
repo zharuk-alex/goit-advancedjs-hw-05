099e435fc41bcfb9189a3b5d2fd0058d460e131a
zharuk.alex <zzharuk@gmail.com>
2024-06-19 11:22:20 +0300

    setup github actions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { concatenation } from "./concatenation";
+import { concatenation } from './concatenation';
 
-const button = document.querySelector("button")!;
-const input = document.querySelector("input")!;
+const button = document.querySelector('button')!;
+const input = document.querySelector('input')!;
 if (button && input) {
-  button.addEventListener("click", () => {
-    concatenation(input.value, "hello!");
+  button.addEventListener('click', () => {
+    concatenation(input.value, 'hello!');
   });
 }
